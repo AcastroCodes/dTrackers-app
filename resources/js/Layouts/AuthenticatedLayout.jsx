6bf649bad6a0f4a4955e18db32c032387489e3c1
dTraker Admin <admin@dtraker.com>
2026-06-08 16:22:28 -0400

    Remove heart icon to exactly match requested copyright string

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -160,7 +160,7 @@ export default function AuthenticatedLayout({ header, children }) {
                             </nav>
                             <div className="credits ml-auto">
                                 <span className="copyright">
-                                    © {new Date().getFullYear()}, made with <i className="fa fa-heart heart"></i> Aristides Castro
+                                    © {new Date().getFullYear()}, made with Aristides Castro
                                 </span>
                             </div>
                         </div>
